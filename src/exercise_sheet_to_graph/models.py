61c57bb956c4ba22a37473c23a4e1f0cd04a15ce
1Lorenzo0 <75297432+1Lorenzo0@users.noreply.github.com>
2024-10-11 16:56:29 +0200

    Create models.py

---
+++ src/exercise_sheet_to_graph/models.py
@@ -0,0 +1,23 @@
+from typing import List
+from datetime import datetime
+import pydantic
+
+
+class Volume(pydantic.BaseModel):
+    weight: int = 0
+    reps: int = 0
+    ts: str = str(datetime.now())
+
+
+class Exercise(pydantic.BaseModel):
+    name: str = ""
+    volumes: List[Volume] = []
+
+
+class SheetPerson(pydantic.BaseModel):
+    name: str = ""
+    exercises: List[Exercise] = []
+
+
+class SheetPeople(pydantic.BaseModel):
+    people: List[SheetPerson] = []
